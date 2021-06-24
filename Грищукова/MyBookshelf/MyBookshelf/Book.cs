ef92f417eed393eef3013a9df568c7a8a0eb29ae
Daria0304-gr <72433055+Daria0304-gr@users.noreply.github.com>
2021-06-24 12:24:10 +0900

    Add files via upload

---
+++ Грищукова/MyBookshelf/MyBookshelf/Book.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyBookshelf
+{
+     class Book
+    {
+		// Поле Id	
+		public int idbooks { get; set; }
+		// Название книги
+		public string Title { get; set; }
+		// Жанр книги
+		public string Genre { get; set; }
+		// Автор книги 
+		public string Author { get; set; }
+		// Год издания
+		public int DateCreate { get; set; }
+		// Описание книги
+		public string Description { get; set; }
+		
+		
+	}
+}
